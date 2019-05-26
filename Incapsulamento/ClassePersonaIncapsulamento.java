c5c03772fd8b72176206054926c237747bb8c4e8
Pinghie <fabio.melasi@gmail.com>
2019-05-26 12:19:52 +0200

    Add files via upload

---
+++ Incapsulamento/ClassePersonaIncapsulamento.java
@@ -0,0 +1,31 @@
+package com.fabiomelasi.primoprogetto.OOP.Introduzione.Incapsulamento;
+
+public class ClassePersonaIncapsulamento {
+    private String nome;
+    private int anni;
+    private char sesso;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnni() {
+        return anni;
+    }
+
+    public void setAnni(int anni) {
+        this.anni = anni;
+    }
+
+    public char getSesso() {
+        return sesso;
+    }
+
+    public void setSesso(char sesso) {
+        this.sesso = sesso;
+    }
+}
---
+++ Incapsulamento/MainClass.java
@@ -0,0 +1,13 @@
+package com.fabiomelasi.primoprogetto.OOP.Introduzione.Incapsulamento;
+
+public class MainClass {
+    public static void main(String args[])
+    {
+        ClassePersonaIncapsulamento persona1 = new ClassePersonaIncapsulamento();
+        persona1.setNome("Mario");
+        persona1.setAnni(26);
+        persona1.setSesso('m');
+
+        System.out.println("Nome: " + persona1.getNome() + " Anni: " + persona1.getAnni() + " Sesso: " + persona1.getSesso());
+    }
+}
