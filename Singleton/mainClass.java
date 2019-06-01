a2ad1f5eee2a6cd7c45a04a0a323fb74e018756e
Pinghie <fabio.melasi@gmail.com>
2019-06-01 11:14:34 +0200

    Create mainClass.java

---
+++ Singleton/mainClass.java
@@ -0,0 +1,9 @@
+package com.fabiomelasi.primoprogetto.OOP.Singleton;
+
+public class mainClass {
+    public static void main(String args[])
+    {
+        Singleton object = Singleton.getInstance();
+        object.DiciQualcosa();
+    }
+}
